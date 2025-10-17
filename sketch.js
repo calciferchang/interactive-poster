edad4f29e454fce683e51dbb64d523d7afeba2fb
calcifer chang <c@lcifer.com>
2025-10-16 21:04:29 -0400

    gaussian distribution implementation

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
 const APEX = {}
-const ANGLES = [-25, -23, -20, -16, -13, -11, -6.5, 0, 5.25, 10, 13, 18, 22, 24, 27]
-
+const ANGLES = [-25, -23, -20, -16, -13, -11, -6.5, 0, 5.25, 10, 13, 18, 22, 24, 25.5]
 function setup() {
   createCanvas(400, 400);
   APEX.x = width / 2
@@ -14,12 +13,18 @@ function setup() {
 function draw() {
 }
 
+function keyPressed() {
+  stroke(255)
+  let randomAngle = floor(constrain(randomGaussian(8, 4), 0, 16))
+  print(randomAngle)
+  drawLineFromAngle(ANGLES[randomAngle])
+}
 function drawLineFromAngle(angleDegrees) {
   const angle = radians(angleDegrees + 90); // to make angles read from relationship to center line
   const y1 = height + 100;
   const dy = y1 - APEX.y;  // vertical distance to travel
   const distance = dy / sin(angle);  // total distance along the line
   const x1 = APEX.x + distance * cos(angle);
-  
+
   line(APEX.x, APEX.y, x1, y1);
 }
